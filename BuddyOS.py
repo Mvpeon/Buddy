9d6df6cc0cd6dbe343035bf22bd309db37477f40
Jørgen <31282843+aasanjorgen@users.noreply.github.com>
2019-05-22 19:26:02 +0200

    Design endringer, koderydding og halvveis fungerende memory spill
    i memory spillet må du trykke på steder som ikke stemmer overens med det grafiske, men funker ellers

--- BuddyOS.py
+++ BuddyOS.py
@@ -1,26 +1,24 @@
 import tkinter as tk
-#import MemoryGame
 import pygame
 pygame.init()
+#import os
 
 LARGE_FONT = ("Verdana", 22)
 MEDIUM_FONT = ("Verdana", 14)
 
-
-class SeaofBTCapp(tk.Tk):
+class BuddyOS(tk.Tk):
 
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
+
         container = tk.Frame(self)
-
         container.pack(side="top", fill="both", expand=True)
-
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
 
-        for F in (StartPage, MG, Kommuniser, MG1, LoadingScreen):
+        for F in (Meny, SpillMeny, Kommuniser, MemorySpill, LoadingScreen, Quiz):
             frame = F(container, self)
 
             self.frames[F] = frame
@@ -40,110 +38,157 @@ class LoadingScreen(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
 
+        self.configure(background="white")
+
         photoLoading = tk.PhotoImage(file="loading.png")
         imgLoading = tk.Label(self, anchor="s", image=photoLoading)
         imgLoading.image = photoLoading
 
-        button1 = tk.Button(self, text="MENY", command=lambda: controller.show_frame(StartPage))
+        button1 = tk.Button(self, text="MENY", command=lambda: controller.show_frame(Meny))
         button1.configure(image=photoLoading, bg='white', width="800", height="480")
         button1.pack()
 
-        self.configure(background="white")
-
-class StartPage(tk.Frame):
+class Meny(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
+
+        self.configure(background="white")
+
+        photoKom = tk.PhotoImage(file="kommknapp.png")
+        photoLaer = tk.PhotoImage(file="laerknapp.png")
+        photoSpill = tk.PhotoImage(file="spillknapp.png")
+
+        imgKom = tk.Label(self, anchor="s", image=photoKom)
+        imgLaer = tk.Label(self, anchor="s", image=photoLaer)
+        imgSpill = tk.Label(self, anchor="s", image=photoSpill)
+
+        imgKom.image = photoKom
+        imgLaer.image = photoLaer
+        imgSpill.image = photoSpill
 
         labelTittel = tk.Label(self, text="MENY", font=LARGE_FONT)
-        buttonKom = tk.Button(self, text="KOMMUNISER", command=lambda: controller.show_frame(Kommuniser))
-        buttonLær = tk.Button(self, text="LÆR DEG", command=lambda: controller.show_frame(StartPage))
-        buttonSpill = tk.Button(self, text="SPILL", command=lambda: controller.show_frame(MG))
+        buttonKom = tk.Button(self, image=photoKom, command=lambda: controller.show_frame(Kommuniser))
+        buttonLær = tk.Button(self, image=photoLaer, command=lambda: controller.show_frame(Meny))
+        buttonSpill = tk.Button(self, image=photoSpill, command=lambda: controller.show_frame(SpillMeny))
 
         labelTittel.configure(background="white")
-        buttonKom.configure(background='purple', font=MEDIUM_FONT, height=2, width=20, fg='white')
-        buttonLær.configure(background='purple', font=MEDIUM_FONT, height=2, width=20, fg='white')
-        buttonSpill.configure(background='purple', font=MEDIUM_FONT, height=2, width=20, fg='white')
-
         labelTittel.pack(pady=10, padx=10)
-        buttonKom.pack(padx=5, pady=5)
-        buttonLær.pack(padx=5, pady=5)
-        buttonSpill.pack(padx=5, pady=5)
 
-        self.configure(background="white")
+        buttonKom.place(x=110, y=150)
+        buttonLær.place(x=310, y=150)
+        buttonSpill.place(x=510, y=150)
+
+        photoWifi = tk.PhotoImage(file="wifi.png")
+        imgWifi = tk.Label(self, anchor="s", image=photoWifi)
+        imgWifi.image = photoWifi  # keep a reference!
+        imgWifi.config(background="white")
+        imgWifi.place(x=740, y=5)
 
 
-class MG(tk.Frame):
+class SpillMeny(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
+
+        self.configure(background="white")
 
         labelTittel = tk.Label(self, text="SPILL", font=LARGE_FONT)
-
-        photoMario = tk.PhotoImage(file="mario.png")
-        photoLuigi = tk.PhotoImage(file="luigi.png")
-        photoToad = tk.PhotoImage(file="toad.png")
-
-        imgMario = tk.Label(self, anchor="s", image=photoMario)
-        imgLuigi = tk.Label(self, anchor="w", image=photoLuigi)
-        imgToad = tk.Label(self, anchor="w", image=photoToad)
-
-        imgMario.image = photoMario  # keep a reference!
-        imgLuigi.image = photoLuigi
-        imgToad.image = photoToad
-
-        buttonMeny = tk.Button(self, command=lambda: controller.show_frame(StartPage))
-        luigi = tk.Button(self, command=lambda: controller.show_frame(MG1))
-        toad = tk.Button(self, command=lambda: controller.show_frame(MG))
-
         labelTittel.configure(background="white")
-        buttonMeny.configure(background='orange', height=2, width=20, fg='white', text="MENY", font="FONT_MEDIUM")
-        luigi.config(image=photoLuigi)
-        toad.config(image=photoToad)
-        imgMario.config(background="white")
-
         labelTittel.pack(pady=10, padx=5)
-        buttonMeny.pack(padx=5, pady=5)
-        luigi.place(x=100, y=200, width=128, height=128)
-        toad.place(x=600, y=200, width=128, height=128)
-        imgMario.place(x=300, y=160)
 
-        self.configure(background="white")
+        #photoMario = tk.PhotoImage(file="mario.png")
+        #imgMario = tk.Label(self, anchor="s", image=photoMario)
+        #imgMario.image = photoMario  # keep a reference!
+        #imgMario.config(background="white")
+        #imgMario.place(x=300, y=160)
 
+        photoMemory = tk.PhotoImage(file="memorytile.png")
+        imgMemory = tk.Label(self, anchor="w", image=photoMemory)
+        imgMemory.image = photoMemory
+        buttonMemory = tk.Button(self, command=lambda: controller.show_frame(MemorySpill))
+        buttonMemory.config(image=photoMemory)
+        buttonMemory.place(x=70, y=140, width=300, height=300)
 
-class MG1(tk.Frame):
+        photoQuiz = tk.PhotoImage(file="quiz.png")
+        imgQuiz = tk.Label(self, anchor="w", image=photoQuiz)
+        imgQuiz.image = photoQuiz
+        buttonQuiz = tk.Button(self, command=lambda: controller.show_frame(Quiz))
+        buttonQuiz.config(image=photoQuiz)
+        buttonQuiz.place(x=430, y=140, width=300, height=300)
+
+        photoReturn = tk.PhotoImage(file="returnknapp.png")
+        imgReturn = tk.Label(self, anchor="s", image=photoReturn)
+        imgReturn.image = photoReturn
+        buttonReturn = tk.Button(self, image=photoReturn, command=lambda: controller.show_frame(Meny))
+        buttonReturn.place(x=10, y=10)
+
+        photoWifi = tk.PhotoImage(file="wifi.png")
+        imgWifi = tk.Label(self, anchor="s", image=photoWifi)
+        imgWifi.image = photoWifi  # keep a reference!
+        imgWifi.config(background="white")
+        imgWifi.place(x=740, y=5)
+
+class MemorySpill(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
 
-        button1 = tk.Button(self, text="MENY", font=MEDIUM_FONT, command=lambda: controller.show_frame(StartPage))
+        self.configure(background="white")
 
-        button1.configure(background='orange', height=2, width=20, fg='white')
+        photoReturn = tk.PhotoImage(file="returnknapp.png")
+        imgReturn = tk.Label(self, anchor="s", image=photoReturn)
+        imgReturn.image = photoReturn
+        buttonReturn = tk.Button(self, image=photoReturn, command=lambda: controller.show_frame(SpillMeny))
+        buttonReturn.place(x=10, y=10)
 
-        button1.pack()
+        photoWifi = tk.PhotoImage(file="wifi.png")
+        imgWifi = tk.Label(self, anchor="s", image=photoWifi)
+        imgWifi.image = photoWifi  # keep a reference!
+        imgWifi.config(background="white")
+        imgWifi.place(x=740, y=5)
 
-        #import MemoryGame
+        from MemoryGame import MemGame
+        x = MemGame(self)
+        x.pack()
+
+class Quiz(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+
+        photoReturn = tk.PhotoImage(file="returnknapp.png")
+        imgReturn = tk.Label(self, anchor="s", image=photoReturn)
+        imgReturn.image = photoReturn
+        buttonReturn = tk.Button(self, image=photoReturn, command=lambda: controller.show_frame(SpillMeny))
+        buttonReturn.place(x=10, y=10)
+
+        photoWifi = tk.PhotoImage(file="wifi.png")
+        imgWifi = tk.Label(self, anchor="s", image=photoWifi)
+        imgWifi.image = photoWifi  # keep a reference!
+        imgWifi.config(background="white")
+        imgWifi.place(x=740, y=5)
 
         self.configure(background="white")
 
-        #MemoryGame.canvas.pack()
+        # quiz spill
 
 class Kommuniser(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
 
+        self.configure(background="white")
+
         labelTittel = tk.Label(self, text="KOMMUNISER", font=LARGE_FONT)
         labelTittel.pack(pady=10, padx=10)
-
-        button1 = tk.Button(self, text="MENY", font=MEDIUM_FONT, command=lambda: controller.show_frame(StartPage))
-
         labelTittel.configure(background="white")
-        button1.configure(background='orange', height=2, width=20, fg='white')
 
-        button1.pack(pady=5)
-
-        self.configure(background="white")
+        photoReturn = tk.PhotoImage(file="returnknapp.png")
+        imgReturn = tk.Label(self, anchor="s", image=photoReturn)
+        imgReturn.image = photoReturn
+        buttonReturn = tk.Button(self, image=photoReturn, command=lambda: controller.show_frame(Meny))
+        buttonReturn.place(x=10, y=10)
 
         hei_sound = pygame.mixer.Sound("hei.wav")
         heter_sound = pygame.mixer.Sound("heter.wav")
@@ -157,9 +202,15 @@ class Kommuniser(tk.Frame):
         buttonB.configure(background='purple', font=MEDIUM_FONT, height=2, width=20, fg='white')
         buttonC.configure(background='purple', font=MEDIUM_FONT, height=2, width=20, fg='white')
 
-        buttonA.pack(pady=5)
-        buttonB.pack(pady=5)
-        buttonC.pack(pady=5)
+        buttonA.place(x=275, y=150)
+        buttonB.place(x=275, y=230)
+        buttonC.place(x=275, y=310)
+
+        photoWifi = tk.PhotoImage(file="wifi.png")
+        imgWifi = tk.Label(self, anchor="s", image=photoWifi)
+        imgWifi.image = photoWifi  # keep a reference!
+        imgWifi.config(background="white")
+        imgWifi.place(x=740, y=5)
 
         def hei():
             pygame.mixer.Sound.play(hei_sound)
@@ -173,5 +224,6 @@ class Kommuniser(tk.Frame):
             pygame.mixer.Sound.play(spille_sound)
             pygame.mixer.music.stop()
 
-app = SeaofBTCapp()
+
+app = BuddyOS()
 app.mainloop()
